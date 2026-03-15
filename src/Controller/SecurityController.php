8951452471df45a0f594b00edeba87e5c16d6ff1
haninalimi <alimihanin716@gmail.com>
2026-03-15 14:59:58 +0100

    feat: correction le login -admin

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -5,18 +5,50 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class SecurityController extends AbstractController
 {
     #[Route('/login', name: 'app_login', methods: ['GET'])]
     public function login(): Response
     {
+        if ($this->getUser()) {
+            return $this->redirectToRoute('event_index');
+        }
         return $this->render('auth/login.html.twig');
     }
 
     #[Route('/register', name: 'app_register', methods: ['GET'])]
     public function register(): Response
     {
+        if ($this->getUser()) {
+            return $this->redirectToRoute('event_index');
+        }
         return $this->render('auth/register.html.twig');
+    }
+
+    #[Route('/logout', name: 'app_logout')]
+    public function logout(): void
+    {
+        throw new \LogicException('Géré par le firewall Symfony.');
+    }
+
+    #[Route('/admin/login', name: 'admin_login', methods: ['GET', 'POST'])]
+    public function adminLogin(AuthenticationUtils $authUtils): Response
+    {
+        if ($this->isGranted('ROLE_ADMIN')) {
+            return $this->redirectToRoute('admin_dashboard');
+        }
+
+        return $this->render('admin/login.html.twig', [
+            'last_username' => $authUtils->getLastUsername(),
+            'error'         => $authUtils->getLastAuthenticationError(),
+        ]);
+    }
+
+    #[Route('/admin/logout', name: 'admin_logout')]
+    public function adminLogout(): void
+    {
+        throw new \LogicException('Géré par le firewall Symfony.');
     }
 }
